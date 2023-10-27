12a3e076b06b34b3c6ae8da969e7872767cfb6f8
Vishal Sharma <785vishal443@gmail.com>
2023-10-27 20:57:10 +0530

    Update Counter.js

--- app/Counter.js
+++ app/Counter.js
@@ -1,11 +1,13 @@
 'use client'
-import React from "react";
+import React, { useContext } from "react";
+import { MealsContext } from "./MealsProvider";
 
 const Counter = () => {
-
+    const { meals } = useContext(MealsContext);
+    const remaingMeals = meals.filter((meal) => !meal.ticked).length; // it checking whether item is ticked or not // let say intitially it will give 3 after pne ,meal clicked it will give 2
     return (
         <div>
-            <h3>Meals Remaining: remaingMeals</h3>
+            <h3>Meals Remaining: {remaingMeals}</h3>
         </div>
     )
 }
